9c1d0a545746fd038fa530ad7af6ac0debd3576d
Vladislav Klimenko <sunsingerus@gmail.com>
2020-06-04 00:52:13 +0300

    env: ostype

--- dev/go_build_config.sh
+++ dev/go_build_config.sh
@@ -18,8 +18,36 @@ GIT_SHA=$(cd "${CUR_DIR}"; git rev-parse --short HEAD)
 # 2020-01-02 12:34:56
 NOW=$(date "+%FT%T")
 
+# linux
+# windows
+# GOOS=""
+# amd64
+# GOARCH=""
+
+if [[ "$OSTYPE" == "cygwin" ]]; then
+    # cygwin
+    GOOS="windows"
+elif [[ "$OSTYPE" == "msys" ]]; then
+    # mingw
+    GOOS="windows"
+elif [[ "$OSTYPE" == "win32" ]]; then
+    # windows
+    GOOS="windows"
+else
+    GOOS="linux"
+fi
+
 # Client binary name can be specified externally
-CLIENT_BIN="${CLIENT_BIN:-${SRC_ROOT}/dev/bin/client}"
+if [[ "${GOOS}" == "windows" ]]; then
+    if [[ -z "${CLIENT_BIN}" ]]; then
+        CLIENT_BIN="${SRC_ROOT}/dev/bin/client.exe"
+    fi
+else
+    if [[ -z "${CLIENT_BIN}" ]]; then
+        CLIENT_BIN="${SRC_ROOT}/dev/bin/client"
+    fi
+fi
+
 CLIENT_BUILDER_SCRIPT="go_build_client.sh"
 
 # Where modules kept
